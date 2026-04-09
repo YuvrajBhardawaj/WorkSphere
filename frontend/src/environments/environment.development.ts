95e584bddf78e154779418d99c8355e7f35aaf73
YuvrajBhardawaj <yuvrajbhardawaj31018@gmail.com>
2026-04-10 00:07:17 +0530

    deployment changes

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,5 +1,4 @@
 export const environment = {
-  supabaseUrl: 'https://qrnzcvdsxhjeugzihupn.supabase.co',
-  supabaseKey: 'sb_publishable_WHe7LoeblKA7E2_ak2UkIw_1ZtBzj-z',
+  production: false,
   backendUrl: 'http://localhost:3000/api'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,3 @@
 export const environment = {
-  supabaseUrl: 'https://qrnzcvdsxhjeugzihupn.supabase.co',
-  supabaseKey: 'sb_publishable_WHe7LoeblKA7E2_ak2UkIw_1ZtBzj-z',
-  backendUrl: 'http://localhost:3000/api'
+  backendUrl: 'https://worksphere-nx4g.onrender.com/api'
 };
